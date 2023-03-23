0697098f4fcf546399634d6f4f43a29ebb2c4a76
RevenMaster <henriqueesteves2004@gmail.com>
2023-03-23 14:26:42 +0000

    Some keymaps changes

--- init.lua
+++ init.lua
@@ -54,8 +54,6 @@ vim.g.maplocalleader = " "
 --   normal_mode = "n",
 --   insert_mode = "i",
 --   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
 --   command_mode = "c",
 
 -- Normal --
@@ -71,9 +69,9 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- Navigate buffers with `control + j/k`
+keymap('n', '<C-j>', ':bnext<CR>', opts)
+keymap('n', '<C-k>', ':bprevious<CR>', opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
@@ -88,11 +86,6 @@ keymap("i", "kj", "<ESC>", opts)
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
-
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
 
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", { desc = "NvimTree" })
 keymap("n", "<leader>s", ":Telescope find_files<cr>", { desc = "Telescope Find Files (search)" })
@@ -251,7 +244,8 @@ require('lazy').setup({
 
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-
+    
+  -- Verifies if the plugin can be installed safely without compiling errors
     build = 'make',
     cond = function()
       return vim.fn.executable 'make' == 1
